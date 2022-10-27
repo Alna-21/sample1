b10cb2973371b22735b14473600fbedc1a328f66
Alna-21 <116424281+Alna-21@users.noreply.github.com>
2022-10-27 18:02:32 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,17 @@
+import "./input.css"
+import React from 'react'
+
+const Input = ({text}) => {
+  return (
+    
+        <div className="text" >
+        <h1>{text}</h1>
+      
+        </div>
+        
+      
+    
+  )
+}
+
+export default Input
