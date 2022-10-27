a9c593db5f353cddb68301803430b9dae3910a5f
Alna-21 <116424281+Alna-21@users.noreply.github.com>
2022-10-27 19:20:20 +0530

    Delete Button.js

--- Button.js
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-const Button=({symbol,color,onClick})=>{
-    return(
-        <div 
-        onClick={()=>onClick(symbol)}
-        className="button-style" style={{backgroundColor:color}}>{symbol}</div>
-    );
-};
-export default Button;
